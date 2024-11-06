44625e1c4a2c87ee6f3148a66f5f55313c0f0bfc
yousef-moustafa <ymoustafa005@gmail.com>
2024-11-06 20:28:19 +0200

    Added child classes: Attendee.java and Speaker.java

---
+++ Attendee.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Attendee extends User{
+    private List<String> registeredSessionIDs;
+    private String personalizedScheduleID;
+    private String certificateID;
+    private String feedbackID;
+
+    public Attendee(String userID, String userName, String email, Date registrationDate, UserRole userRole, List<String> registeredSessionIDs, String personalizedScheduleID, String certificateID, String feedbackID) {
+        super(userID, userName, email, registrationDate, userRole);
+        this.registeredSessionIDs = registeredSessionIDs;
+        this.personalizedScheduleID = personalizedScheduleID;
+        this.certificateID = certificateID;
+        this.feedbackID = feedbackID;
+    }
+}
+
---
+++ Speaker.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Speaker extends User{
+    private String bio;
+    private List<String> associatedSessionIDs;
+
+    public Speaker(String userID, String userName, String email, Date registrationDate, UserRole userRole, String bio, List<String> associatedSessionIDs) {
+        super(userID, userName, email, registrationDate, userRole);
+        this.bio = bio;
+        this.associatedSessionIDs = associatedSessionIDs;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+}
