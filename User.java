686cc869558165ed18dd23e010858cbf75f0d614
yousef-moustafa <ymoustafa005@gmail.com>
2024-11-06 20:15:54 +0200

    Added UserRole.java and basic getters and setters for User class

--- User.java
+++ User.java
@@ -5,11 +5,43 @@ public class User {
     private String userName;
     private String email;
     private Date registrationDate;
+    private UserRole userRole;
 
-    public User(String userID, String userName, String email, Date registrationDate) {
+    public User(String userID, String userName, String email, Date registrationDate, UserRole userRole) {
         this.userID = userID;
         this.userName = userName;
         this.email = email;
         this.registrationDate = registrationDate;
+        this.userRole = userRole;
+    }
+
+    // Getters for all attributes
+    public String getUserID() {
+        return userID;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Date getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public UserRole getUserRole() {
+        return userRole;
+    }
+
+    // Setters needed
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
---
+++ UserRole.java
@@ -0,0 +1,5 @@
+public enum UserRole {
+    ATTENDEE,
+    SPEAKER,
+    MANAGER
+}
